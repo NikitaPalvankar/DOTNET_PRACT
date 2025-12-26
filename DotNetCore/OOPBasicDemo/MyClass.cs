f945c82a66c9a5b9190b5b7142c77b8ffed4cac4
Nikita Palvankar <nikita2001palvankar@gmail.com>
2025-12-27 00:32:39 +0530

    DOT NET

---
+++ DotNetCore/OOPBasicDemo/MyClass.cs
@@ -0,0 +1,11 @@
+﻿
+namespace OOPBasic
+{
+    internal class MyClass
+    {
+        public void getMessage()
+        {
+            Console.WriteLine("Hello from MyClass in Namespace OOPBasic");
+        }
+    }
+}
---
+++ DotNetCore/OOPBasicDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿using OOPBasic;
+using System;
+namespace OOPBasicDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Int32 x = 10;
+            System.Int32 y = 20;
+            Console.WriteLine("Using System.Int32 -> x: " + x+" y: "+y);
+
+
+            Int32 a = 100;
+            Int32 b = 200;
+            Console.WriteLine("Using Int32 -> a: " + a + " b: " + b);
+
+
+            int p = 101;
+            int q = 500;
+            Console.WriteLine("Using int -> p: " + p + " q: " + q);
+
+
+            //OOPBasic.MyClass. = new MyClass();
+            MyClass obj = new MyClass();
+            obj.getMessage();
+
+        }
+    }
+
+   
+}
